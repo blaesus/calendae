fb77f093e233ef944aa0ce396949d2164de14d77
Andy Shu <shu@immux.com>
2019-09-20 21:46:33 +0800

    Add picker

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { Picker, StyleSheet, Text, View } from "react-native";
+import { Calendar } from "expo/build/removed.web";
 
 const styles = StyleSheet.create({
     container: {
@@ -10,21 +11,59 @@ const styles = StyleSheet.create({
     },
     date: {
         fontSize: 20,
+    },
+    calendarPicker: {
+        flex: 1,
+    },
+    today: {
+        flex: 1,
+        height: 100,
     }
 });
 
+type Calendar = "auc" | "gonghe"
+
+function getYearOffset(calendar: Calendar): number {
+    switch (calendar) {
+        case "auc": {
+            return 753;
+        }
+        case "gonghe": {
+            return 841;
+        }
+    }
+}
+
 export default function App() {
+    const [calendar, setCalendar] = useState<Calendar>("gonghe");
     const now = new Date();
     const startThisYear = new Date(now.getFullYear(), 0, 0);
     const delta = +now - +startThisYear;
     const oneDay = 1000 * 60 * 60 * 24;
     const day = Math.floor(delta / oneDay);
 
-    let year = now.getUTCFullYear() + 841; // Gonghe
+    const year = now.getUTCFullYear() + getYearOffset(calendar);
+
 
     return (
         <View style={styles.container}>
-            <Text style={styles.date}>{day}/{year}</Text>
+            <View style={styles.calendarPicker}>
+                <Text>Calendar</Text>
+                <Picker
+                    selectedValue={calendar}
+                    style={{width: 100}}
+                    onValueChange={(value) => setCalendar(value)}
+                    mode="dropdown"
+                >
+                    <Picker.Item label="A.U.C." value="auc" />
+                    <Picker.Item label="Gonghe" value="gonghe" />
+                </Picker>
+            </View>
+
+            <View style={styles.today}>
+                <Text>Today</Text>
+                <Text style={styles.date}>{day}/{year}</Text>
+            </View>
         </View>
     );
 }
